40fa82804d1cd3f43f255868baa6e5a31d3322b0
Damian Sloane <damian@capacitorlabs.com.au>
2021-07-03 00:03:10 +1000

    Work in progress

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ data "aws_iam_policy_document" "github_deployment" {
       "cloudfront:GetDistributionConfig",
       "cloudfront:ListInvalidations"
     ]
-    resources = "*"
+    resources = ["*"]
   }
 }
 
